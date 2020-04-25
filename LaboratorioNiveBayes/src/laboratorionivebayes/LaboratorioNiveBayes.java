22605da2eca5d10565059276c85e1a01ab35e8d0
LeslyDubon <leslykarinadubon@yahoo.com>
2020-04-25 10:35:40 -0600

    Descartar conectores

--- LaboratorioNiveBayes/src/laboratorionivebayes/LaboratorioNiveBayes.java
+++ LaboratorioNiveBayes/src/laboratorionivebayes/LaboratorioNiveBayes.java
@@ -7,7 +7,9 @@ package laboratorionivebayes;
 
 import de.daslaboratorium.machinelearning.classifier.Classifier;
 import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -20,6 +22,7 @@ public class LaboratorioNiveBayes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String[] valores = {"aquellas","aquellos","aquella","aquel","esas","esos","ese","esa","estas","estos","esta","este","lo","del","al","unas","unos","una","un","tal","según","ante","pues","porque","que","así","en","sin","a","de","es","pero","hasta","incluso","encima","igualmente","asimismo","también","la", "el", "de", "los", "las" , "por", "mientras", "para", "con", "es", "ademas", "y", "sobre", "cabe", "destacar", "modo" , "mismo"};
         Scanner scanner = new Scanner(System.in);
         Classifier<String, String> bayes = new BayesClassifier<String, String>();
         
@@ -30,12 +33,12 @@ public class LaboratorioNiveBayes {
         while(!salir.toUpperCase().equals("SI"))
         {
             System.out.println("Ingrese una frase");
-            String[] frase = scanner.nextLine().split("\\s");
-
+            String[] frase = scanner.nextLine().toLowerCase().split("\\s");
+            String[] frasesinConectores = saltarConectores(valores,frase);
             System.out.println("Ingrese la etiqueta de la frase ingresada");
             String etiqueta = scanner.nextLine();       
             
-            bayes.learn(etiqueta, Arrays.asList(frase));      
+            bayes.learn(etiqueta, Arrays.asList(frasesinConectores));      
             
             String respuesta = "x";
             
@@ -61,13 +64,14 @@ public class LaboratorioNiveBayes {
         while(!salir.toUpperCase().equals("NO"))
         {
             System.out.println("Ingrese una frase");
-            String[] frase = scanner.nextLine().split("\\s");
+            String[] frase = scanner.nextLine().toLowerCase().split("\\s");
+            String[] frasesinConectores = saltarConectores(valores,frase);
                       
             //Etiqueta
-            System.out.println(bayes.classify(Arrays.asList(frase)).getCategory());
+            System.out.println(bayes.classify(Arrays.asList(frasesinConectores)).getCategory());
             //Detalles
             System.out.println(((BayesClassifier<String, String>) bayes).classifyDetailed(
-            Arrays.asList(frase)));
+            Arrays.asList(frasesinConectores)));
             
             String respuesta = "x";
             
@@ -89,6 +93,18 @@ public class LaboratorioNiveBayes {
         // the learn method) are stored in a queue with the size given
         // here and used to classify unknown sentences.
         //bayes.setMemoryCapacity(500);
+    }
+    
+    public static String[] saltarConectores(String[] saltar, String[] valores){
+        List<String> saltarList = new ArrayList(Arrays.asList(saltar));
+        List<String> valoresList = new ArrayList(Arrays.asList(valores));
+        for(int i = 0; i < valoresList.size(); i++){
+            String valor = valoresList.get(i);
+            if(saltarList.contains(valor)){
+                valoresList.remove(i);
+            }
+        }
+        return valoresList.toArray(new String[0]);
     }
     
 }
